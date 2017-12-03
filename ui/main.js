28ff47b0884467f6969d5e979cc8004e5a504daa
tirthankar12121997 <b116062@iiit-bh.ac.in>
2017-12-03 21:13:18 +0530

    added functionality to image and added click to change value of body

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,19 @@ console.log('Loaded!');
 alert('Welcome to the App');
 
 var img = document.getElementById('madi');
+var element = document.getElementById('content');
+
+element.onclick = function(){
+	element.innerHTML = 'New Value';
+}
+
+var posx = 0;
+
+function moveRight(){
+	posx = posx + 5;
+	img.style.marginLeft = posx + 'px';
+}
 
 img.onclick = function(){
-    img.style.marginRight = '100px';
+    var interval = setInterval(moveRight,50);
 };

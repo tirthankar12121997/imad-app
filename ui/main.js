466144e7da3216e90d95fd277b5b745d35c02ce3
tirthankar12121997 <33565134+tirthankar12121997@users.noreply.github.com>
2017-12-03 17:32:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,8 @@
 console.log('Loaded!');
-console.log('Loaded!');
 alert('Welcome to the App');
 
 var img = document.getElementById('madi');
+
+img.onclick = function(){
+    img.style.marginLeft = '100px';
+};

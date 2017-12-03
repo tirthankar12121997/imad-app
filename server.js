719ede47df5a16c0cd5d0adff8ef19d0fc0f3e44
tirthankar12121997 <b116062@iiit-bh.ac.in>
2017-12-03 13:54:44 +0530

    removed the 3 articles and put them in server.js and modified style.css

--- server.js
+++ server.js
@@ -5,20 +5,95 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles = { 
+	articleone : {
+	title: 'Article-1',
+	heading: 'Article-one',
+	date: '1 Dec 2017',
+	content: `
+			<p>
+				This is the content for article-one.This is the content for article-one.This is the content for article-one.This is the content for article-one.This is the content for article-one.
+				This is the content for article-one.
+				This is the content for article-one.This is the content for article-one.This is the content for article-one.
+			</p>
+			<p>
+				This is the content for article-one.This is the content for article-one.This is the content for article-one.This is the content for article-one.This is the content for article-one.
+				This is the content for article-one.
+				This is the content for article-one.This is the content for article-one.This is the content for article-one.
+			</p>
+			<p>
+				This is the content for article-one.This is the content for article-one.This is the content for article-one.This is the content for article-one.This is the content for article-one.
+				This is the content for article-one.
+				This is the content for article-one.This is the content for article-one.This is the content for article-one.
+			</p>`
+    },
+    articletwo : {
+	title: 'Article-2',
+	heading: 'Article-two',
+	date: '12 Dec 2017',
+	content: `
+			<p>
+				This is the content for article-two.
+			</p>`
+    },
+    articlethree : {
+	title: 'Article-3',
+	heading: 'Article-three',
+	date: '13 Dec 2017',
+	content: `
+			<p>
+				This is the content for article-three.
+			</p>`
+    }
+};
+
+function createtemplate(data){
+	title = data.title;
+	heading = data.heading;
+	date = data.date;
+	content = data.content;
+	var htmltemplate = `
+	<html>
+	<head>
+		<title>
+			${title}
+		</title>
+		<meta charset="viewport" content="width=device-width,initial-scale=1" />
+		<link href="ui/style.css" rel="stylesheet"/>
+	</head>
+	<body>
+		<div class = "container">
+		<div>
+		<a href = "/">Home</a>
+		</div>
+		<hr/>
+		<h3>${heading}</h3>
+		<div>
+			${date}
+		</div>
+		<div>
+			${content}
+		</div>
+		</div>
+	</body>
+	</html>`;
+	return htmltemplate;
+}
+
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname,'ui','index.html'));
 });
 
 app.get('/article-1',function(req,res){
-  res.sendFile(path.join(__dirname,'ui','article-1.html'));
+  res.send(createtemplate(articles.articleone));
 });
 
 app.get('/article-2',function(req,res){
-  res.sendFile(path.join(__dirname,'ui','article-2.html'));
+  res.send(createtemplate(articles.articletwo));
 });
 
 app.get('/article-3',function(req,res){
-  res.sendFile(path.join(__dirname,'ui','article-3.html'));
+  res.send(createtemplate(articles.articlethree));
 });
 
 app.get('/ui/style.css', function (req, res) {

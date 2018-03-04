dd1e24c4189b0d6bc65394c04255c89b0f83ede5
tirthankar12121997 <33565134+tirthankar12121997@users.noreply.github.com>
2018-03-04 16:13:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config = {
   database: 'tirthankarnayak',
   host: 'http://db.imad.hasura-app.io',
   port: '5432',
-  password: process.env.DB_PASSWORD
+  password: 'db-tirthankarnayak-67978'
 };
 
 var app = express();

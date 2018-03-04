9f503c21de4601fc30a5cce0e66000efcfc87dc3
tirthankar12121997 <33565134+tirthankar12121997@users.noreply.github.com>
2018-03-04 19:10:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.get('/test-db', function(req, res){
         }
         else
         {
-            res.send(JSON.stringify(result));    
+            res.send(JSON.stringify(result[0]));    
         }
     });
 });
